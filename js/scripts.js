25d0daf894c06e097fd3002c0ec727e45fb8d33e
Debra Fabbri <debrafabbri@gmail.com>
2022-08-04 12:44:55 -0300

    Navbar creation

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,10 @@
 const openAndCloseAccordion = () => {
   alert ('do something')
 }
+
+const iconMenu = document.querySelector(".header__navbar__hamburger-menu");
+const navBar = document.querySelector('.header__navbar');
+
+iconMenu.addEventListener('click', () => {
+  navBar.classList.toggle("change");
+})
