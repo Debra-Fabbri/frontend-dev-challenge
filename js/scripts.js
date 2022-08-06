2a3711ff0b22cba44d29e96f56c970f11a916af0
Debra Fabbri <debrafabbri@gmail.com>
2022-08-05 22:10:27 -0300

    added 'testimonial' section

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,29 @@ const navBar = document.querySelector('.header__navbar');
 iconMenu.addEventListener('click', () => {
   navBar.classList.toggle("change");
 })
+
+/* SLIDER TESTIMONIAL */
+let slider = document.querySelector('.slider-contenedor');
+let sliderInd = document.querySelectorAll('.slider-test');
+let contador = 1;
+let tamWidth = sliderInd[0].clientWidth;
+let intervalo = 10000;
+
+window.addEventListener("resize", function(){
+  tamWidth = sliderInd[0].clientWidth;
+});
+
+setInterval(function tiempo(){
+  slides();
+}, intervalo);
+
+
+function slides(){
+  slider.style.transform = 'translate(' + ( - tamWidth * contador) + 'px)';
+  slider.style.transition = 'transform 1s';
+  contador++;
+
+  if(contador == sliderInd.length){
+    contador = 0;
+  }
+}
