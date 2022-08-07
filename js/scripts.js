72f8216fde2f5357a6568d80c55418212a33e430
Debra Fabbri <debrafabbri@gmail.com>
2022-08-07 00:49:19 -0300

    added 'modal'

--- js/scripts.js
+++ js/scripts.js
@@ -33,6 +33,39 @@ iconMenu.addEventListener('click', () => {
 
 
 /* -------------------------------------------------------------------------------------------------------- */
+/* modal*/
+const openModal = document.querySelector('.hero__cta');
+const modal = document.querySelector('.modal');
+const closeModal = document.querySelector('.modal__close');
+
+openModal.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal.classList.add('modal--show');
+});
+
+closeModal.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal.classList.remove('modal--show');
+});
+
+
+/* modal2*/
+const openModal2 = document.querySelector('.hero__cta2');
+const modal2 = document.querySelector('.modal2');
+const closeModal2 = document.querySelector('.modal__close2');
+
+openModal2.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal2.classList.add('modal2--show');
+});
+
+closeModal2.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal2.classList.remove('modal2--show');
+});
+
+
+/* -------------------------------------------------------------------------------------------------------- */
 /* SLIDER TESTIMONIAL */
 
 let slider = document.querySelector('.slider-contenedor');
