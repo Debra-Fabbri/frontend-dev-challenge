9ba74b1b16ccd790275b501b0d4c5f08760cb4f3
Debra Fabbri <debrafabbri@gmail.com>
2022-08-07 17:57:43 -0300

    hero added + scss file modifications

--- js/scripts.js
+++ js/scripts.js
@@ -20,6 +20,7 @@ const fadeOut = () => {
 
 window.addEventListener('load', fadeOut);
 
+
 /* -------------------------------------------------------------------------------------------------------- */
 /* Hamburger Menu */
 const iconMenu = document.querySelector(".header__navbar__hamburger-menu");
@@ -33,11 +34,64 @@ iconMenu.addEventListener('click', () => {
 
 
 /* -------------------------------------------------------------------------------------------------------- */
+/* SLIDER */
+/* Getting the elements from the DOM. */
+const upBtn = document.querySelector('.up-button'),
+	downBtn = document.querySelector('.down-button'),
+	sideBar = document.querySelector('.sidebar'),
+	mainSlide = document.querySelector('.main-slide'),
+	slidesCnt = sideBar.querySelectorAll('div');
+let activeIdx = 0;
+
+/* Setting the top property of the sideBar element to the value of the expression. */
+sideBar.style.top = `-${(slidesCnt.length - 1) * 100}vh`
+
+/**
+ * If the direction is up, increase the activeIdx by 1, if it's greater than or equal to the length of
+ * the slidesCnt array, set it to 0, then set the mainSlide and sideBar's transform to the activeIdx
+ * times 100vh, otherwise, if the direction is down, decrease the activeIdx by 1, if it's less than 0,
+ * set it to the length of the slidesCnt array minus 1, then set the mainSlide and sideBar's transform
+ * to the activeIdx times 100vh.
+ * @param direction - up or down
+ */
+const changeSlide = (direction) => {
+	switch (direction) {
+		case 'up': {
+			activeIdx++
+			if (activeIdx >= slidesCnt.length) activeIdx = 0
+			mainSlide.style.transform = `translateY(-${activeIdx * 100}vh)`
+			sideBar.style.transform = `translateY(${activeIdx * 100}vh)`
+			break
+		}
+		case 'down': {
+			console.log(activeIdx)
+			activeIdx--
+			if (activeIdx < 0) activeIdx = slidesCnt.length - 1
+			mainSlide.style.transform = `translateY(-${activeIdx * 100}vh)`
+			sideBar.style.transform = `translateY(${activeIdx * 100}vh)`
+			break
+		}
+	}
+};
+
+/* Listening for a click event on the upBtn and downBtn elements and then it is calling the
+function `changeSlide()` with the parameter 'up' and 'down' respectively. */
+upBtn.addEventListener('click', () => changeSlide('up'));
+downBtn.addEventListener('click', () => changeSlide('down'));
+
+/* Calling the function `changeSlide()` every 10 seconds. */
+const myTimer = setInterval(() => changeSlide('up'), 10000);
+
+
+/* -------------------------------------------------------------------------------------------------------- */
 /* modal*/
+/* Getting the elements from the DOM. */
 const openModal = document.querySelector('.hero__cta');
 const modal = document.querySelector('.modal');
 const closeModal = document.querySelector('.modal__close');
 
+/* Listening for a click event on the openModal element and then it is adding the class
+'modal--show' to the modal element. */
 openModal.addEventListener('click', (e) => {
 	e.preventDefault();
 	modal.classList.add('modal--show');
@@ -49,7 +103,7 @@ closeModal.addEventListener('click', (e) => {
 });
 
 
-/* modal2*/
+/* modal2 */
 const openModal2 = document.querySelector('.hero__cta2');
 const modal2 = document.querySelector('.modal2');
 const closeModal2 = document.querySelector('.modal__close2');
@@ -123,13 +177,13 @@ accordionBtns.forEach((accordion) => {
 
 
 /* -------------------------------------------------------------------------------------------------------- */
-/*   Contact-us   
-	form validation
-*/
+/*   Contact-us   form validation   */
 
+/* Getting the form and the inputs. */
 const formulario = document.getElementById('formulario');
 const inputs = document.querySelectorAll('#formulario input');
 
+/* Creating a regular expression for each of the fields. */
 const expresiones = {
 	name: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
 	lastName: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
@@ -137,6 +191,7 @@ const expresiones = {
 	phone: /^\d{7,14}$/ // 7 a 14 numeros.
 }
 
+/* An object that contains the names of the fields of the form and the value false. */
 const campos = {
 	name: false,
 	lastName: false,
@@ -144,6 +199,11 @@ const campos = {
 	phone: false
 }
 
+/**
+ * It takes an event object and a switch statement that checks the name of the input field and then
+ * calls a function that takes a regular expression, the event object and the name of the input field.
+ * @param e - is the event object
+ */
 const formValidation = (e) => {
 	switch (e.target.name) {
 		case "name":
@@ -161,6 +221,12 @@ const formValidation = (e) => {
 	}
 }
 
+/**
+ * It validates the input field and changes the class of the input field and the icon.
+ * @param expresion - the regular expression
+ * @param input - the input element
+ * @param campo - the name of the input field
+ */
 const validarCampo = (expresion, input, campo) => {
 	if (expresion.test(input.value)) {
 		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
@@ -179,11 +245,16 @@ const validarCampo = (expresion, input, campo) => {
 	}
 }
 
+/* Adding an event listener to each input element. */
 
 inputs.forEach((input) => {
 	input.addEventListener('keyup', formValidation);
 	input.addEventListener('blur', formValidation);
 });
+
+/* Listening for a submit event on the form and then it is checking if all the fields are valid and if
+the checkbox is checked. If it is true, then it is resetting the form and showing a message. If it
+is false, then it is showing an error message. */
 
 formulario.addEventListener('submit', (e) => {
 	e.preventDefault();
